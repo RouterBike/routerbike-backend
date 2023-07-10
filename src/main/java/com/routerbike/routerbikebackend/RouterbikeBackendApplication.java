914714fa54de6577c9990bb53a9154933b597596
pedrohdsimoes <63207385+pedrohdsimoes@users.noreply.github.com>
2023-07-10 13:44:22 +0100

    backend and postgres

---
+++ src/main/java/com/routerbike/routerbikebackend/RouterbikeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.routerbike.routerbikebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterbikeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterbikeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routerbike/routerbikebackend/RouterbikeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routerbike.routerbikebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterbikeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
